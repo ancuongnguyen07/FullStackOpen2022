efdba677faf7ab774266974628e4049682f62c4d
Cuong Nguyen <ancuongnguyen07@gmail.com>
2022-08-03 17:44:26 +0300

    [part2] phonebook: 2.15-2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,14 @@
 // import logo from './logo.svg';
 // import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import personService from './services/persons';
 
-const Persons = ({persons}) => persons.map((person) => (
-  <p key={person.name}>{person.name} {person.number}</p>
+const Persons = ({persons, deleteFunc}) => persons.map((person) => (
+  <div key={person.id}>
+    <p>{person.name} {person.number}</p>
+    <button onClick={() => deleteFunc(person)}>delete</button>
+  </div>
+  
 )) 
 
 const Filter = ({ val, onChangeFunc}) => <p>filter shown with <input value={val} onChange={onChangeFunc}/></p>
@@ -23,12 +28,24 @@ const PersonForm = ({onSubmitFunc, name, nameOnChangeFunc, number, numberOnChang
 )
 
 function App() {
+  // displayed list of persons as a result of filtering and adding
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '39-44-5323523' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    // { name: 'Arto Hellas', number: '39-44-5323523' },
+    // { name: 'Ada Lovelace', number: '39-44-5323523' },
+    // { name: 'Dan Abramov', number: '12-43-234345' },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+
+  // fixed list of db-contained persons
+  const [dbPersons, setDbPersons] = useState([])
+
+  // fetching data from json-server
+  useEffect(() => {
+    personService.getAll().then(initialPersons => {
+      setPersons(initialPersons)
+      setDbPersons(initialPersons)
+    })
+  }, [])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -47,16 +64,42 @@ function App() {
     const existedName = persons.find(p => p.name === newName)
     const existedNumber = persons.find(p => p.number === newNumber)
     if (existedName !== undefined){
-      window.alert(`${newName} is already added to phonebook`)
+      // Already existed person
+      // window.alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${existedName.name} is already added to phonebook,`
+            + ` replace the old number with a new one?`)){
+              const updatedPerson = {...existedName, number: newNumber}
+              const id = existedName.id
+              personService.update(id, updatedPerson).then(returnedPerson => {
+                const updatedPersons = dbPersons.map(p => p.id === id ? returnedPerson : p)
+                setDbPersons(updatedPersons)
+                setPersons(updatedPersons)
+                setNewName('')
+                setNewNumber('')
+              })
+             }
     } else if (existedNumber !== undefined){
       window.alert(`${newNumber} is already added to phonebook`)
     } 
     else{
-      const copy = [...persons]
-      const newPersons = copy.concat({ name: newName, number: newNumber})
-      setPersons(newPersons)
-      setNewName('')
-      setNewNumber('')
+      const newPerson = {name: newName, number: newNumber, id: persons.length + 1}
+      personService.create(newPerson).then(returnedPerson => {
+        const updatedPersons = dbPersons.concat(returnedPerson)
+        setPersons(updatedPersons)
+        setDbPersons(updatedPersons)
+        setNewName('')
+        setNewNumber('')
+        setSearchedName('')
+      })
+    }
+  }
+
+  const handleClickDeleteButt = person => {
+    if (window.confirm(`Delete ${person.name} ?`)){
+      personService.deletePerson(person.id)
+      const updatedPersons = dbPersons.filter(p => p.id !== person.id)
+      setDbPersons(updatedPersons)
+      setPersons(updatedPersons)
     }
   }
 
@@ -68,8 +111,7 @@ function App() {
   const searchName = (event) => {
     const text = event.target.value
     setSearchedName(text)
-    const copy = [...persons]
-    const result = copy.filter(p => p.name.toLowerCase().includes(text.toLowerCase()))
+    const result = dbPersons.filter(p => p.name.toLowerCase().includes(text.toLowerCase()))
     setPersons(result)
   }
 
@@ -81,7 +123,7 @@ function App() {
       <PersonForm name={newName} nameOnChangeFunc={handleTypeName} 
         number={newNumber} numberOnChangeFunc={handleTypeNumber} onSubmitFunc={handleSubmitForm} />
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={persons} deleteFunc={handleClickDeleteButt}/>
     </div>
   )
 }
