4b38908fe7f3ec1346dd586b46651156f6560d49
Cuong Nguyen <ancuongnguyen07@gmail.com>
2023-01-02 17:08:44 +0200

    [part 4] 4.18

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -10,6 +10,7 @@ app.use(express.json())
 
 const blogRouter = require('./controllers/blog')
 const userRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 // const Blog = require('./models/blog')
@@ -26,6 +27,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
